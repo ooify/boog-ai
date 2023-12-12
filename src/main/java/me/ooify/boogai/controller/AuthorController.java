c04a82325e701a3c0c220839e6395edeca84db6c
jonsan <gaigaisang@outlook.com>
2023-12-12 17:19:16 +0800

    up AuthorController

--- src/main/java/me/ooify/boogai/controller/AuthorController.java
+++ src/main/java/me/ooify/boogai/controller/AuthorController.java
@@ -1,13 +1,27 @@
 package me.ooify.boogai.controller;
 
+import cn.dev33.satoken.annotation.SaCheckRole;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
+import me.ooify.boogai.dto.author.AuthorDTO;
+import me.ooify.boogai.entity.Author;
+import me.ooify.boogai.entity.User;
+import me.ooify.boogai.service.impl.AuthorServiceImpl;
+import me.ooify.boogai.utils.Result;
+import org.modelmapper.ModelMapper;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author jonsan
@@ -17,5 +31,75 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/author")
 public class AuthorController {
+    @Resource
+    private AuthorServiceImpl authorService;
+
+    @Resource
+    private ModelMapper modelMapper;
+
+    @GetMapping("/{id}")
+    public Result getAuthorById(@PathVariable Long id) {
+        Author author = authorService.getById(id);
+        AuthorDTO map = modelMapper.map(author, AuthorDTO.class);
+        return Result.ok("查询成功")
+                .setData(map);
+    }
+
+    @GetMapping
+    public Result getAllAuthors(@RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum,
+                                @RequestParam(value = "pageSize", defaultValue = "10") Integer pageSize,
+                                @RequestParam(value = "AuthorName", required = false) String AuthorName,
+                                @RequestParam(value = "sortField", defaultValue = "created_at") String sortField,
+                                @RequestParam(value = "sortOrder", defaultValue = "desc") String sortOrder) {
+        pageNum = Math.max(pageNum, 1);
+        pageSize = Math.min(pageSize, 100);
+        if (!sortField.equals("created_at") && !sortField.equals("updated_at")) {
+            sortField = "created_at";
+        }
+        if (!sortOrder.equals("asc") && !sortOrder.equals("desc")) {
+            sortOrder = "desc";
+        }
+        Page<Author> page = new Page<>(pageNum, pageSize);
+        QueryWrapper<Author> queryWrapper = new QueryWrapper<>();
+        queryWrapper.like(AuthorName != null, "name", AuthorName)
+                .orderByAsc("asc".equalsIgnoreCase(sortOrder), sortField)
+                .orderByDesc("desc".equalsIgnoreCase(sortOrder), sortField);
+
+        Page<Author> paged = authorService.page(page, queryWrapper);
+        IPage<AuthorDTO> convert = paged.convert(author -> modelMapper.map(author, AuthorDTO.class));
+        return Result.ok("查询成功")
+                .setData(convert);
+    }
+
+    @SaCheckRole("admin")
+    @PostMapping
+    public Result createAuthor(@Valid @RequestBody AuthorDTO authorDTO, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return Result.error(Objects.requireNonNull(bindingResult.getFieldError()).getDefaultMessage());
+        }
+        Author author = modelMapper.map(authorDTO, Author.class);
+        authorService.save(author);
+        return Result.ok("创建成功")
+                .setData(author);
+    }
+
+    @SaCheckRole("admin")
+    @PutMapping
+    public Result updateAuthor(@Valid @RequestBody AuthorDTO authorDTO, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return Result.error(Objects.requireNonNull(bindingResult.getFieldError()).getDefaultMessage());
+        }
+        Author author = modelMapper.map(authorDTO, Author.class);
+        authorService.updateById(author);
+        return Result.ok("更新成功")
+                .setData(author);
+    }
+
+    @SaCheckRole("admin")
+    @DeleteMapping("/{id}")
+    public Result deleteAuthor(@PathVariable Long id) {
+        authorService.removeById(id);
+        return Result.ok("删除成功");
+    }
 
 }
--- src/main/java/me/ooify/boogai/dto/author/AuthorDTO.java
+++ src/main/java/me/ooify/boogai/dto/author/AuthorDTO.java
@@ -1,6 +1,9 @@
 package me.ooify.boogai.dto.author;
 
 
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,9 +13,11 @@ import java.util.Date;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class AuthorDTO{
-    private Long authorId;
-    private String authorName;
-    private Date createdAt;
-    private Date updatedAt;
+public class AuthorDTO {
+    @NotNull(message = "id不能为空")
+    @Digits(integer = 19, fraction = 0, message = "id不合法")
+    private Long id;
+    @NotNull(message = "name不能为空")
+    @Size(min = 1, max = 100, message = "name长度不合法")
+    private String name;
 }
